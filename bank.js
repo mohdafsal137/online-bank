608c1ebdd9582c21420ffd06a937d7736e04a09b
mohdafsal137 <mohdafsal137@gmail.com>
2023-11-22 00:51:24 +0530

    new

---
+++ bank.js
@@ -0,0 +1,114 @@
+//register
+
+function register(){
+    const bank={
+        uname:uname.value,
+        acno:acno.value,
+        pswd:pswd.value,
+        balance:0
+    }
+    if(bank.uname==""||bank.acno==""||bank.pswd==""){
+alert('fill all Datas')
+    }
+    else{
+        if(bank.acno in localStorage){
+            alert('account already existed')
+        }
+        else{
+            localStorage.setItem(bank.acno,JSON.stringify(bank))
+            alert('new account created sucessfully')
+            window.location="./login.html"
+        }
+    }
+
+}
+
+//login
+
+function signin(){
+    let acno=document.getElementById('acno').value
+    let pswd=document.getElementById('pswd').value
+    if(acno==""||pswd==""){
+        alert('enter all details')
+    }
+    else{
+        if(acno in localStorage){
+            let bankk=JSON.parse(localStorage.getItem(acno))
+            if(pswd==bankk.pswd){
+                localStorage.setItem('login',JSON.stringify(bankk))
+                alert('login sucessful')
+
+                window.location="./home.html"
+            }
+            else{
+                alert('incorrect password')
+            }
+        }
+        else{
+            alert('account does not found. register new account')
+            window.location="./register.html"
+        }
+    }
+}
+
+//deposit and withdraw
+
+function deposit(){
+    amount=amt.value;
+    amount=Math.floor(amount);
+    pswd=deposit_pswd.value;
+    let Acc = JSON.parse(localStorage.getItem(pswd));
+    if(amount == ""||pswd==""){
+        alert("please fill the details");
+    }
+    else{
+        if(pswd in localStorage){
+            if(pswd==Acc.pswd){
+                Acc.balance+=amount;
+                deposit_balance.innerHTML =`<p class="text-success ">Deposit Successful , <span class="text-secondary">Current Balance Is : ₹${Acc.balance}</span></p>`;
+                localStorage.setItem(Acc.pswd,JSON.stringify(Acc))
+            }
+        }
+        else{
+            alert("incorrect password")
+        }
+    }
+}
+
+//withdraw
+
+function widh(){
+    amount = widh_amt.value;
+    amount=Math.floor(amount)
+    pswd=widh_pswd.value
+
+    if(amount=="" ||pswd.value==""){
+alert('fill all details')
+    }
+    else{
+        if(pswd in localStorage){
+            let Acc= JSON.parse(localStorage.getItem(pswd))
+            if(pswd==Acc.pswd){
+                if(Acc.balance> amount){
+                    Acc.balance -= amount;
+                    widh_balance.innerHTML = `<p class="text-success mx-5 mt-3">Withdraw Successfull ,<span class="text-secondary"> Current Balance Is : ₹ ${Acc.balance}</span></p>`;
+                    localStorage.setItem(Acc.pswd,JSON.stringify(Acc))
+                }
+                else{
+
+                }
+            }
+        }
+        else{
+            alert("incorrect password")
+        }
+    }
+
+}
+
+//log out
+function logout() {
+    localStorage.clear()
+    window.location="./bank.html"
+}
+
